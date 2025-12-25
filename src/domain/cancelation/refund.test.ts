f85ab080a4b31cb9b97a2cd3a36a782ccf163e52
MathzBrk <matheusvbborges@hotmail.com>
2025-12-25 19:00:55 -0300

    fix: Update refund test descriptions for clarity and consistency

--- src/domain/cancelation/refund.test.ts
+++ src/domain/cancelation/refund.test.ts
@@ -1,21 +1,21 @@
 import { FullRefund, NoRefund, PartialRefund } from "./refund";
 
 describe("Refund", () => {
-	it("it should return 100 for FullRefund", () => {
+	it("it should return 0 as the price for FullRefund", () => {
 		const fullRefund = new FullRefund();
-		const refundAmount = fullRefund.calculateRefund(100);
-		expect(refundAmount).toBe(0);
+		const price = fullRefund.calculateRefund(100);
+		expect(price).toBe(0);
 	});
 
-	it("it should return 0 for NoRefund", () => {
+	it("it should return 100 as the price for NoRefund", () => {
 		const noRefund = new NoRefund();
-		const refundAmount = noRefund.calculateRefund(100);
-		expect(refundAmount).toBe(100);
+		const price = noRefund.calculateRefund(100);
+		expect(price).toBe(100);
 	});
 
-	it("it should return 50 for PartialRefund", () => {
+	it("it should return 50 as the price for PartialRefund", () => {
 		const partialRefund = new PartialRefund();
-		const refundAmount = partialRefund.calculateRefund(100);
-		expect(refundAmount).toBe(50);
+		const price = partialRefund.calculateRefund(100);
+		expect(price).toBe(50);
 	});
 });
